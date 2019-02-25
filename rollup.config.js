f29ee93cf5e5773e4b5a4abcb32ea6935a746ecf
Steve Sewell <sewell.steve@gmail.com>
2019-02-25 08:54:25 -0800

    add module and umd configs for webpack/parcel/browserify/etc support

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json'
 
-export default {
+const deafultConfig = {
   input: 'src/index.js',
+  plugins: [resolve()],
+}
+
+const iife = {
+  ...deafultConfig,
   output: {
     file: 'dist/ses-shim.js',
     exports: 'named',
@@ -9,5 +15,42 @@ export default {
     name: 'SES',
     sourcemap: true,
   },
-  plugins: [resolve()],
-};
+}
+
+const modules = {
+  ...deafultConfig,
+  output: [{
+      file: './dist/cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: './dist/esm.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  // Do not bundle up external depenencies for module builds
+  external: Object.keys(pkg.dependencies),
+  plugins: [
+    // Only resolve and bundle local files, not external dependencies
+    resolve({
+      only: [/^\.{0,2}\//]
+    })
+  ]
+}
+
+const umd = {
+  ...deafultConfig,
+  output: {
+    file: 'dist/umd.js',
+    name: 'SES',
+    format: 'umd',
+    sourcemap: true,
+    amd: {
+      id: 'SES'
+    }
+  }
+}
+
+export default [iife, modules, umd];
