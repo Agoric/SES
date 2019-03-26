099ca4aa65c813f082a76344cdab771efbd42a56
Kate Sills <katelynsills@gmail.com>
2019-03-25 19:04:32 -0700

    Add the dist files to SES, add bundler integration tests

---
+++ integration-test/test/utility/test-bundler.js
@@ -0,0 +1,50 @@
+/* eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies */
+import puppeteer from 'puppeteer-core';
+/* eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies */
+import test from 'tape-promise/tape';
+
+import path from 'path';
+
+const runBrowserTests = async indexFile => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: 'google-chrome',
+  });
+  const page = await browser.newPage();
+  page.on('pageerror', err => {
+    console.log(err);
+  });
+
+  let numTests;
+  let numPass;
+  page.on('console', msg => {
+    if (msg.text().includes('# tests')) {
+      [numTests] = msg
+        .text()
+        .split(' ')
+        .slice(-1);
+    }
+    if (msg.text().includes('# pass')) {
+      [numPass] = msg
+        .text()
+        .split(' ')
+        .slice(-1);
+    }
+  });
+  await page.goto(`file:${path.join(__dirname, indexFile)}`);
+  await page.title();
+  await browser.close();
+  return { numTests, numPass };
+};
+
+const testBundler = (bundlerName, indexFile) => {
+  test(`SES works with ${bundlerName}`, t => {
+    runBrowserTests(indexFile).then(({ numTests, numPass }) => {
+      t.notEqual(numTests, undefined);
+      t.equal(numTests, numPass);
+      t.end();
+    });
+  });
+};
+
+export default testBundler;
